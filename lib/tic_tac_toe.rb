cc3ef1ef06ec0a24307d3611ee1aee1773424877
Chris Allen <chrisallen415@gmail.com>
2020-05-19 17:44:34 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,49 @@
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    # Rows
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    # Columns
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    # Diagionals
+    [0,4,8],
+    [2,4,6],
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    index = input.to_i - 1
+  end
+
+  def move(@board, index, player)
+    @board[index] = player
+  end
+
+  def position_taken?(@board, index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+
+
+end
